84c99ebaf3c499ec994c21dd37ee435ca4480432
Chanrat_Sophal <chanrothsophal@gmail.com>
2025-03-29 14:45:07 +0700

    update main

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+log_action(){
+	bash Features/log_file.sh "$1"
+}
+
 while true; do
 	echo "=============================="
 	echo "File & Directory Mangement"
@@ -16,13 +20,13 @@ while true; do
 	read choice
 
 	case $choice in
-		1) bash Features/list_file_dir.sh ;;
-		2) bash Features/backup.sh ;;
-		3) bash Features/count_file.sh ;;
-		4) bash Features/disk_usage.sh ;;
-		5) bash Features/search_file.sh ;;
-		6) bash Features/compress.sh ;;
-		7) echo "Exiting!"; exit 0 ;;
+		1) log_action "Listed files"; bash Features/list_file_dir.sh ;;
+		2) log _action "Backup files"; bash Features/backup.sh ;;
+		3) log_action "Counted files"; bash Features/count_file.sh ;;
+		4) log _action "Checked disk usage"; bash Features/disk_usage.sh ;;
+		5) log_action "Searched for a file"; bash Features/search_file.sh ;;
+		6) log_action "Compressed files"; bash Features/compress.sh ;;
+		7) log_action "Exited the program"; echo "Exiting!"; exit 0 ;;
 		*) echo "Invalid options. Please enter the existing options!" ;;
 	esac
 done
