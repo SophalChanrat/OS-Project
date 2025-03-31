05cb6a0fb53775a6d5008a7c362b0a7ea5e50ce7
SSONita <sarsovannita4@gmail.com>
2025-03-31 11:44:45 +0700

    Update disk_usage.sh

--- Features/disk_usage.sh
+++ Features/disk_usage.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 log_file="script.log"
-
-disk_usage(){
-	read -p "Enter the directory path: " dir
-	du -ah "$dir"
-	echo "$(date) - $(whoami) - Checked disk usage of $dir" >> "$LOG_FILE"
-}
+read -p "Enter the directory path: " dir
+du -ah "$dir"
+echo "$(date) - $(whoami) - Checked disk usage of $dir" >> "$LOG_FILE"
